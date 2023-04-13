b82b1b0315f66a332cd551e3dabcb3217f12c9c4
Dmitri S <ser.dmitri@gmail.com>
2023-04-12 22:03:20 -0400

    Increase test coverage

--- src/repositories/restrictions.repository.test.ts
+++ src/repositories/restrictions.repository.test.ts
@@ -1,6 +1,10 @@
 import { NullDietDay } from "../data";
 import { MealEnum } from "../types";
-import { repetitionBetweenDays, repetitionWithinMeals } from "./restrictions";
+import {
+  recipeQuantityLimit,
+  repetitionBetweenDays,
+  repetitionWithinMeals,
+} from "./restrictions";
 
 describe("restrictions repository", () => {
   describe("repetitionWithinMeals", () => {
@@ -41,6 +45,24 @@ describe("restrictions repository", () => {
       );
       expect(actualValue).toEqual(1);
     });
+
+    it("should penalize going over the day's recipe limit", async () => {
+      const dietDay = {
+        ...NullDietDay,
+        meals: {
+          ...NullDietDay.meals,
+          [MealEnum.BREAKFAST]: {
+            totalRecipes: 1,
+            recipeIds: ["0", "1", "2"],
+          },
+        },
+      };
+      const actualWeight = await repetitionWithinMeals(
+        dietDay,
+        MealEnum.BREAKFAST
+      );
+      expect(actualWeight).toEqual(-2);
+    });
   });
 
   describe("repetitionBetweenDays", () => {
@@ -79,6 +101,44 @@ describe("restrictions repository", () => {
 
       expect(nullDayValue).toEqual(1);
       expect(actualValue).toEqual(-1);
+    });
+  });
+
+  describe("recipeQuantityLimit", () => {
+    it("should penalize for going over the daily recipe limit", () => {
+      const currentDietDay = {
+        ...NullDietDay,
+        meals: {
+          ...NullDietDay.meals,
+          [MealEnum.BREAKFAST]: {
+            totalRecipes: 2,
+            recipeIds: ["1", "2", "3"],
+          },
+        },
+      };
+      const actualWeight = recipeQuantityLimit(
+        currentDietDay,
+        MealEnum.BREAKFAST
+      );
+      expect(actualWeight).toEqual(-1);
+    });
+
+    it("should return 1 if no recipes are needed for the meal", () => {
+      const currentDietDay = {
+        ...NullDietDay,
+        meals: {
+          ...NullDietDay.meals,
+          [MealEnum.BREAKFAST]: {
+            totalRecipes: 0,
+            recipeIds: [],
+          },
+        },
+      };
+      const actualWeight = recipeQuantityLimit(
+        currentDietDay,
+        MealEnum.BREAKFAST
+      );
+      expect(actualWeight).toEqual(1);
     });
   });
 });
